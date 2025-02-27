969d56f91bb8388eef46291851ccbad48d897b10
Elly <hubert.olegario@student.ateneo.edu>
2025-02-27 14:44:47 +0800

    Created the Models in models.py

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=100)
+    ingredient = models.ForeignKey(Ingredient, 
+                                   on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, 
+                               on_delete=models.CASCADE)
