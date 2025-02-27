eca33418a04c17e07422cef611e056c59aa592f7
Elly <hubert.olegario@student.ateneo.edu>
2025-02-27 16:59:53 +0800

    Was able to create a List View using the Recipe model

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import index, recipe_list, recipe1, recipe2
+from .views import index, RecipeListView
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('recipes/list', recipe_list, name='recipe_list'),
-    path('recipe/1', recipe1, name='recipe1'),
-    path('recipe/2', recipe2, name='recipe2'),
+    path('recipes/list', RecipeListView.as_view(), name='list'),
 ]
 
 app_name = "ledger"
--- ledger/views.py
+++ ledger/views.py
@@ -1,75 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def index(request):
-    return HttpResponse('Hello World! This came from the index view')
+from .models import Ingredient, Recipe, RecipeIngredient
 
-def recipe_list(request):
-    ctx = {
-    "recipes": [
-        {
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "/recipe/1"
-        },
-        {
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "/recipe/2"
-        }
-    ]
-    }
-    return render(request, 'recipe_list.html', ctx)
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipe_list.html'
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe_detail.html'
 
 
 def recipe1(request):
