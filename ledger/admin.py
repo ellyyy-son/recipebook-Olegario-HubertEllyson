f847a56864a14f2fe30e8d083710020af1d7f06c
Elly <hubert.olegario@student.ateneo.edu>
2025-02-27 15:29:07 +0800

    Fixed models.py and created an admin panel

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+# registering the model and the admin is what tells
+# Django that admin pages must be generated for the models specified
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
--- ledger/models.py
+++ ledger/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 
-from django.db import models
-
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.name)])
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.name)])
+
+
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
     ingredient = models.ForeignKey(Ingredient, 
-                                   on_delete=models.CASCADE)
+                                   on_delete=models.CASCADE,
+                                   related_name="ingredient")
     recipe = models.ForeignKey(Recipe, 
-                               on_delete=models.CASCADE)
+                               on_delete=models.CASCADE,
+                               related_name="recipe")
