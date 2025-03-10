1220699d1e58b73b59ad25ee56d2f11e613a1674
Elly <hubert.olegario@student.ateneo.edu>
2025-03-10 19:57:25 +0800

    Finished tasks related to log in flow
    - Was able to edit base.html to show things depending if logged in or not
    
    - Finished the forgot password pages and it works
    
    - Created log out functionality

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,7 +5,6 @@ from .views import RecipeListView, RecipeDetailView
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='list'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail'),
-    path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 app_name = "ledger"
