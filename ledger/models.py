002419675dd503cce3fcf455b1794073014a866c
Elly <hubert.olegario@student.ateneo.edu>
2025-03-24 22:26:51 +0800

    Fixed for spec compliance

--- ledger/models.py
+++ ledger/models.py
@@ -37,7 +37,7 @@ class RecipeIngredient(models.Model):
 
 class RecipeImage(models.Model):
     image = models.ImageField(upload_to='images/', null=True)
-    description = models.TextField(max_length=100)
+    description = models.TextField(max_length=255)
     recipe = models.ForeignKey(Recipe,
                                on_delete=models.CASCADE,
                                related_name="images")
