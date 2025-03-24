ca928de50810cb07de383ac633823f15fbf94fbc
Elly <hubert.olegario@student.ateneo.edu>
2025-03-24 15:12:20 +0800

    Fixed errors on the implementation of admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from .models import Ingredient, Recipe, RecipeIngredient, RecipeImage
 
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -13,7 +13,11 @@ class IngredientAdmin(admin.ModelAdmin):
 class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline]
 
+class RecipeImageAdmin(admin.ModelAdmin):
+    model = RecipeImage
+
 
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient)
+admin.site.register(RecipeImage ,RecipeImageAdmin)
